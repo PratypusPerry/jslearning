c0f4a5b2ebae21a9436da77363e53c081ce861f9
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-08-13 19:14:17 +0530

    String Challenges completed

---
+++ challengestr.js
@@ -0,0 +1,11 @@
+var str= "Pratyush"
+console.log(str.charAt(3));
+console.log(str.charCodeAt(2));
+console.log(str.substring(3,7));
+console.log(str.substr(1,5));
+var st= " P, r, t, t, y";
+console.log(st.split(","));
+console.log(st);
+console.log(String.fromCharCode(65,66,67,68,69));
+console.log(str.indexOf("rat"));
+console.log(str.lastIndexOf("tyu"));
