936e94da99c33c5fd526e90b6990e268e6c2664f
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-08-13 21:12:37 +0530

    Date time object

---
+++ datetime.js
@@ -0,0 +1,9 @@
+let cd= new Date();
+console.log(cd);
+console.log(cd.toLocaleString());
+console.log(Date.now());
+let cd1= new Date(2018,11,5);
+console.log(cd1.toLocaleString());
+console.log(cd.getDay());
+var cd2= new Date(1660403214602);
+console.log(cd2.toLocaleString());
