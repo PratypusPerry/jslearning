12578879c297142712a0a4516627351706ce91b6
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-08-13 18:53:04 +0530

    Commited all the array challenges

---
+++ challenge.js
@@ -0,0 +1,16 @@
+//sqareroot of array elements
+let arr=[25,36,49,64,81];
+let newarr= arr.map((en,index,arr) =>{
+    return Math.sqrt(en);
+})
+console.log(newarr);
+//return elements greater than ten after multiplying by two
+let arr1=[2,3,4,5,6,7,8];
+let newarr1= arr1.filter((en,index,arr1)=>{
+    if((en*2)>10)
+    return (en);
+})
+let newarr2= newarr1.map((en,index,newarr1)=>{
+    return (en*2);
+})
+console.log(newarr2);
