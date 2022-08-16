a9831e8b037536f5f8860f797f26751f94b78012
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-08-16 17:02:28 +0530

    Before Advanced and upto ES 2015-20

--- jslearn.js
+++ jslearn.js
@@ -2,7 +2,7 @@ var myname='Pratyush Banerjee';
 console.log(myname);
 console.log(typeof(myname));
 console.log(5 - "A");
-console.log(3**3);
+console.log(3**5);
 var a=Math.floor(16/10);
 console.log(a);
 console.log(Math.sqrt(16));
--- object.js
+++ object.js
@@ -1,17 +1,33 @@
-// myname.getEverything();
+// Nested object, objecct properties
+let Name= "HEY WHAT'S YOUR NAME?"
 let myname = {
-    Name : 'Pratyush Banerjee',
+    [Name] : 'Pratyush Banerjee',
     Age : '19+',
     Education : {
         School : 'The Aryans School',
         College : 'MSIT',
         CurrentS : 'SECOND YEAR BTECH',
-        // getED (){
-            // console.log(`My School is ${myname.Education.School}, College is ${myname.Education.College} and current qualification is ${myname.Education.CurrentS}`);
-            // }, 
+        getED (){
+            console.log(`My School is ${myname.Education.School}, College is ${myname.Education.College} and current qualification is ${myname.Education.CurrentS}`);
+            }, 
         },
         getEverything (){
-            console.log(`My Name is ${myname.Name}, currently I am ${myname.Age} years old and my educational qualifications are My School is ${myname.Education.School}, College is ${myname.Education.College} and current qualification is ${myname.Education.CurrentS}`);
+            console.log(`My Name is ${myname.Name}, currently I am ${myname.Age} years old. `);
+            this.Education.getED();
+
         }
 }
 myname.getEverything();
+console.log(myname);
+console.log(Object.entries(myname));
+console.log(Object.entries(myname.Education));
+//object destructure
+// let {j,k}= myname.Education;
+// console.log(j);
+//spread operators
+var a=[2,3,4,5,6,7];
+var b=[...a,8,9,10];
+var c=[0,1,...b];
+console.log(a);
+console.log(b);
+console.log(c);
